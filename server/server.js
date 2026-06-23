55143a2544fec4ce427e4aeccb8d56a8ccfcedca
elmonickcool <elmonickol@gmail.com>
2026-06-23 17:07:05 +0800

    create mysql connection

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "medical_records",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  console.log("Connected to the database");
+});
