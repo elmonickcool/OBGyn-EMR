ba9883a71ee2e0b7bb720cc3a37eb4c9def2c8d0
elmonickcool <elmonickol@gmail.com>
2026-06-23 20:52:46 +0800

    add view id patient

--- client/src/pages/PatientDetails.jsx
+++ client/src/pages/PatientDetails.jsx
@@ -63,48 +63,41 @@ function PatientDetails() {
   return (
     <Box sx={{ maxWidth: 700, mx: "auto", p: 3 }}>
       <Paper elevation={3} sx={{ p: 3 }}>
-
         {/* Header */}
         <Typography variant="h4" gutterBottom>
           Patient Details
         </Typography>
 
         {/* Patient Info */}
-       <Box sx={{ mb: 3 }}>
-  <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-    <Typography>
-      <strong>ID:</strong> {patient.patient_id}
-    </Typography>
+        <Box sx={{ mb: 3 }}>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Typography>
+              <strong>ID:</strong> {patient.patient_id}
+            </Typography>
 
-    <Typography>
-      <strong>Date:</strong>{" "}
-      {patient.created_at
-        ? new Date(patient.created_at).toLocaleDateString()
-        : "N/A"}
-    </Typography>
-  </Box>
+            <Typography>
+              <strong>Date:</strong>{" "}
+              {patient.created_at
+                ? new Date(patient.created_at).toLocaleDateString()
+                : "N/A"}
+            </Typography>
+          </Box>
 
-  <Typography>
-    <strong>Name:</strong> {patient.first_name} {patient.last_name}
-  </Typography>
+          <Typography>
+            <strong>Name:</strong> {patient.first_name} {patient.last_name}
+          </Typography>
 
-  <Typography>
-    <strong>Age:</strong> {patient.age}
-  </Typography>
-</Box>
+          <Typography>
+            <strong>Age:</strong> {patient.age}
+          </Typography>
+        </Box>
 
         {/* Consultation Section */}
         <Typography variant="h5" gutterBottom>
           Consultation
         </Typography>
 
-       
-        <TextField
-          label="Chief Complaint"
-          multiline
-          rows={5}
-          fullWidth
-        />
+        <TextField label="Chief Complaint" multiline rows={5} fullWidth />
       </Paper>
     </Box>
   );
