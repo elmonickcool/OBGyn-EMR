ef2f13df5c7bf987bcc43cefeee74297f5d2dc5c
elmonickcool <elmonickol@gmail.com>
2026-06-23 17:09:36 +0800

    label a script

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
+// import required modules
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
-
+// create an instance of express
 const app = express();
-
+// set up middleware
 app.use(cors());
 app.use(express.json());
-
+// create a connection to the MySQL database
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
   database: "medical_records",
 });
-
+// connect to the database
 db.connect((err) => {
   if (err) {
     console.error("Error connecting to the database:", err);
