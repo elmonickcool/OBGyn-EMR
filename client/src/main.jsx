7038c8c2c8115c29feea48b56fcb0af301dde234
elmonickcool <elmonickol@gmail.com>
2026-06-24 12:38:06 +0800

    setup mui x

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  ,
-)
+  </LocalizationProvider>
+);
