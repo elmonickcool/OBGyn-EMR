e19790cffbf7a9cbc82d1a90af235d847ecf1c1d
elmonickcool <elmonickol@gmail.com>
2026-08-27 01:48:28 +0800

    refactor server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 
 const express = require("express");
-const mysql = require("mysql2");
 const cors = require("cors");
-
+const db = require("./db");
 const app = express();
 
 require("dotenv").config()
@@ -10,21 +9,6 @@ require("dotenv").config()
 app.use(cors());
 app.use(express.json());
 
-/* ================= DATABASE ================= */
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-    return;
-  }
-  console.log("Connected to database");
-});
 
 /* ================= PATIENTS ================= */
 
